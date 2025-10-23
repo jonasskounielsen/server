d0e843a833ced0e050e29f805cf6b0ee1b958814
nutria-official <nutria.official@proton.me>
2025-10-23 11:53:11 +0200

    Initial commit

--- configuration.nix
+++ configuration.nix
@@ -14,6 +14,7 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ./gnome.nix
   ];
 
   # Use the systemd-boot EFI boot loader.
---
+++ gnome.nix
@@ -0,0 +1,5 @@
+{
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true; # Gnome Display Manager
+  services.xserver.desktopManager.gnome.enable = true; # Enable GNOME
+}
