56acf53b6d42866c219e999aff35374acb5fda84
nutria-official <nutria.official@proton.me>
2025-10-24 10:34:30 +0200

    add: playit-agent package

--- configuration.nix
+++ configuration.nix
@@ -58,9 +58,14 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN4Zqri3qqyHhkboqJefXoW8uDHx55zh4i9k3SYDWx7J jonas@jonas-laptop" # Jonas
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFF/MMrpoPMM2Exj+WazhonE/lTKawPiwc3vJEmXsmH1 SILDE" # Lucas
     ];
-    packages = with pkgs; [
+    packages =
+      with pkgs;
+      [
 
-    ];
+      ]
+      ++ [
+        playit-agent
+      ];
   };
 
   programs = {
