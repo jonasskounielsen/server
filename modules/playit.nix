49cf451611f42a0b5cf9cd1f6b0cb242775f1667
nutria-official <nutria.official@proton.me>
2025-10-27 10:06:13 +0100

    add: systemd-service for starting playit-agent on boot

---
+++ modules/playit.nix
@@ -0,0 +1,12 @@
+{
+  systemd.services.playit = {
+    description = "A systemd-service for starting a playit tunnel.";
+
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
+
+    wantedBy = [ "multi-user.target" ];
+
+    script = "playit-cli";
+  };
+}
