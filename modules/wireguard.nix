f6b9e3b82b24436110311daf12f506aac3a68caf
nutria-official <nutria.official@proton.me>
2026-09-11 12:18:55 +0200

    done:fix:add: killswitch, config-issues

--- modules/wireguard.nix
+++ modules/wireguard.nix
@@ -1,20 +1,47 @@
 { config, ... }:
+
+let
+  vpn_ip = "193.29.107.162";
+  vpn_port = "51820";
+in
+
 {
-  sops.secrets."wireguard/public_key" = { };
-  sops.secrets."wireguard/private_key" = { };
+  sops.secrets."wireguard/private_key" = {
+    restartUnits = [ "wireguard-wg0.service" ];
+  };
 
   networking.wg-quick.interfaces."wg0" = {
-    privateKey = config.sops.secrets."wireguard/private_key";
+    privateKeyFile = config.sops.secrets."wireguard/private_key".path;
     address = "10.2.0.2/32";
     dns = "10.2.0.1";
     autostart = true;
     peers = [ {
-        publicKey = config.sops.secrets."wireguard/public_key";
-        endpoint = "193.29.107.162:51820";
+        publicKey = "sbjnjFtxUz4dxYfNL7WOVf1StMjjAhkiPLCPtVtlhRI=";
+        endpoint = "${vpn_ip}:${vpn_port}";
+        persistentKeepalive = 25;
         allowedIps = [
           "0.0.0.0/0"
           "::/0"
         ];
     } ];
+  };
+  networking.nftables = {
+    enable = true;
+    tables.kill-switch = {
+      family = "inet";  # handles IPv4 AND IPv6 with one table
+      content = ''
+        chain output {
+          type filter hook output priority 0; policy drop;
+
+          oifname "lo" accept
+
+          oifname "wg0" accept
+
+          ip daddr ${vpn_ip} udp dport ${vpn_port} accept
+
+          ct state established,related accept
+        }
+      '';
+    };
   };
 }
--- to-do.md
+++ to-do.md
@@ -2,5 +2,3 @@
 2. Setup webserver.
 3. Setup Minecraft Fabric server.
 4. Host own DNS perhaps with unbound and pihole.
-5. Make wg0 killswitch.
-6. Make secrets work in wg-quick config.
